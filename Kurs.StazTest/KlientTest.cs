0e6312c667950eaf207e958ed8f5e94ec06fe324
PrzemekO <przemyslaw.oglaza@outlook.com>
2020-04-16 01:57:29 +0200

    Pierwszy test jednostkowy ImeNazwisko

---
+++ Kurs.StazTest/KlientTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using KursStaz.BL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kurs.StazTest
+{
+    [TestClass]
+    public class KlientTest
+    {
+        [TestMethod]
+        public void ImieNazwiskoTest()
+        {
+            // Arrange (przygotuj ters)
+
+            Klient klient = new Klient();
+            klient.Imie = "Robert";
+            klient.Nazwisko = "Kowal";
+
+            string oczekiwana = "Robert, Kowal";
+
+            // Act (działaj)
+
+            string aktualna = klient.ImieNazwisko;
+
+            // Assert (potwierdz test)
+
+            Assert.AreEqual(oczekiwana, aktualna);
+
+        }
+    }
+}
---
+++ Kurs.StazTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Kurs.StazTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Kurs.StazTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1c222879-7635-4bec-81d1-397e8b1955ca")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
